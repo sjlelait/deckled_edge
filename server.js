8e1bd9a7267aa28cce2350dc83767f9741b520b4
Sara Le Lait <sjlelait@gmail.com>
2023-11-19 17:48:39 -0700

    fix: edits URI typo

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 // Dependencies
+const mongoose = require('mongoose');
 const express = require('express');
 
 const app = express();
@@ -8,8 +9,19 @@ require("dotenv").config();
 
 const { PORT = 3001, DATABASE_URI } = process.env;
 
+// Database Connection
+mongoose.connect(DATABASE_URI);
+
+mongoose.connection
+    .on('open', () => console.log('You are connected to MongoDB'))
+    .on('close', () => console.log('You are disconnected from MongoDB'))
+    .on('error', (error) => console.log(`MongoDB Error: ${error.message}`));
+
+// Models
+
 // Mount Middleware 
 
+app.use(express.json());
 
 // Mount Routes
 // test route
